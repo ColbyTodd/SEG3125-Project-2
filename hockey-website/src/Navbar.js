892ed298b43867f0c851e7796d2cc25925133c7b
Colby Todd <ctodd083@uottawa.ca>
2023-06-26 22:48:05 -0400

    Built navbar

--- hockey-website/src/Navbar.js
+++ hockey-website/src/Navbar.js
@@ -1,10 +1,17 @@
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <h1>Hockey Website</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/search">Search</a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">
+                <img src="/d" width="30" height="30" class="d-inline-block align-top" alt=""/>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="/">Home </a>
+                    <a class="nav-item nav-link" href="/search">Search</a>
+                </div>
             </div>
         </nav>
     );
