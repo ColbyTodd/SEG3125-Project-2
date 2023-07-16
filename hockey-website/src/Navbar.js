d0dfa8d966ee550a36776248bea8f62b3b3aa370
Colby Todd <ctodd083@uottawa.ca>
2023-07-16 19:36:14 -0400

    Changed logo

--- hockey-website/src/Navbar.js
+++ hockey-website/src/Navbar.js
@@ -4,15 +4,15 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand navbar-dark bg-dark">
             <Link className="navbar-brand" to='/'>
-                <img src="https://previews.123rf.com/images/sergeymastepanov/sergeymastepanov1703/sergeymastepanov170300020/73493596-ice-hockey-logo-vector.jpg" width="30" height="30" className="d-inline-block align-top" alt=""/>
+                <img src={process.env.PUBLIC_URL + '/logo-white.png'} width="30" height="30" className="d-inline-block align-top ms-4" alt="logo"/>
             </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <Link className="nav-item nav-link" to='/'>Home</Link>
-                    <Link className="nav-item nav-link" to="/search">Search</Link>
+                    <Link className="nav-item nav-link active" to='/'>Home</Link>
+                    <Link className="nav-item nav-link active" to="/search">Search</Link>
                 </div>
             </div>
         </nav>
