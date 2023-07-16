200c0fafe673fa3158295a85ba0e3df58d9fd898
Colby Todd <ctodd083@uottawa.ca>
2023-07-16 19:20:19 -0400

    Fixed footer to always be at bottom of the page

--- hockey-website/src/App.js
+++ hockey-website/src/App.js
@@ -21,7 +21,9 @@ function App() {
               <Route path={``} render={ (routerProps) => < Home routerProps={routerProps} />} />
             </Switch>
           </div>
-        <Footer />
+          <div className="footer">
+            <Footer />
+          </div>
       </div>
     </Router>
   );
