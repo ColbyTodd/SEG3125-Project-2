c0a30917e900900629ba828edc4053145f0c00cf
Colby Todd <ctodd083@uottawa.ca>
2023-07-16 18:58:34 -0400

    Added margin to footer

--- hockey-website/src/Footer.js
+++ hockey-website/src/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (  
-        <footer className="bg-dark text-center text-white">
+        <footer className="bg-dark text-center text-white mt-5">
             <div className="text-center p-3">
                 Data gathered from:
                 <a className="text-white" href="https://NHL.com/">NHL.com</a>
