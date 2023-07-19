230c612d25d6b36e1138bdc7b95352fcbf0a098d
Colby Todd <ctodd083@uottawa.ca>
2023-07-18 22:38:21 -0400

    Right aligned language drop down

--- hockey-website/src/Navbar.js
+++ hockey-website/src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand navbar-dark bg-dark">
             <Link className="navbar-brand" to='/'>
-                <img src={process.env.PUBLIC_URL + '/logo-white.png'} width="30" height="30" className="d-inline-block align-top ms-4" alt="logo"/>
+                <img src={process.env.PUBLIC_URL + '/logo-white.png'} width="30" height="30" className="d-inline-block ms-4" alt="logo"/>
             </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -14,6 +14,13 @@ const Navbar = () => {
                     <Link className="nav-item nav-link active" to='/'>Home</Link>
                     <Link className="nav-item nav-link active" to="/search">Search</Link>
                 </div>
+                
+            </div>
+            <div className="navbar-nav navbar-right me-4">
+                <select className="nav-item nav-link active" style={{ float: 'right' }}>
+                        <option style={{color: "black"}} selected>EN</option>
+                        <option style={{color: "black"}}>FR</option>
+                </select>
             </div>
         </nav>
     );
