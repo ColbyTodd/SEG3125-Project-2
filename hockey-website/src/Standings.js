be255bd576ea8f7e6f3108008889adc7eb9371f5
Colby Todd <ctodd083@uottawa.ca>
2023-07-19 17:03:25 -0400

    Made standings screen readable

--- hockey-website/src/Standings.js
+++ hockey-website/src/Standings.js
@@ -7,23 +7,23 @@ const Standings = (props) => {
                 <table className="table">
                     <thead>
                             <tr>
-                                <th style={{width: '300px'}} scope="col">{teams[0].Division}</th>
-                                <th scope="col" alt="Games played">GP</th>
-                                <th scope="col">W</th>
-                                <th scope="col">L</th>
-                                <th scope="col">OTL</th>
-                                <th scope="col">PTS</th>
+                                <th style={{width: '300px'}} scope="col" aria-label={teams[0].Division + "."}>{teams[0].Division}</th>
+                                <th scope="col" aria-label="Games played.">GP</th>
+                                <th scope="col" aria-label="Wins.">W</th>
+                                <th scope="col" aria-label="Losses.">L</th>
+                                <th scope="col" aria-label="Overtime Losses.">OTL</th>
+                                <th scope="col" aria-label="Points.">PTS</th>
                             </tr>
                     </thead>
                     {teams.map((team, index) => (
                         <tbody key={index}>
                             <tr>
-                                <th scope="row">{index + 1} {team.Name}</th>
-                                <td>{team.GP}</td>
-                                <td>{team.W}</td>
-                                <td>{team.L}</td>
-                                <td>{team.OTL}</td>
-                                <td>{team.PTS}</td>
+                                <th scope="row" aria-label={index + 1 + "," + team.Name + ","}>{index + 1} {team.Name}</th>
+                                <td aria-label={team.GP + ","}>{team.GP}</td>
+                                <td aria-label={team.W + ","}>{team.W}</td>
+                                <td aria-label={team.L + ","}>{team.L}</td>
+                                <td aria-label={team.OTL + ","}>{team.OTL}</td>
+                                <td aria-label={team.PTS + ","}>{team.PTS}</td>
                             </tr>
                         </tbody>
                     ))}
