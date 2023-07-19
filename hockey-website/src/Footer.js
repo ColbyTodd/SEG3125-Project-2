7d4bb39f7c45a310a8ad6165b6109c2db051e61c
Colby Todd <ctodd083@uottawa.ca>
2023-07-19 18:23:33 -0400

    Fixed navbar and footer language to change properly

--- hockey-website/src/Footer.js
+++ hockey-website/src/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (  
         <footer className="bg-dark text-center text-white mt-5">
-            <div className="text-center p-3">
-                Need help? Visit our discord:
+            <div className="text-center p-3" id="textDiv">
+                Need help? Visit our discord: 
                 <a className="text-white ps-1" href="https://discord.com/">https://discord.com/</a>
                 <br/>
                 Data gathered from:
--- hockey-website/src/Navbar.js
+++ hockey-website/src/Navbar.js
@@ -1,24 +1,122 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+    const locat = useLocation();
+    var div;
+    // Create the "Need help?" text node
+    var helpText = document.createTextNode("Need help? Visit our discord: ");
+
+    // Create the link element for Discord
+    var discordLink = document.createElement("a");
+    discordLink.href = "https://discord.com/";
+    discordLink.className = "text-white ps-1";
+    discordLink.textContent = "https://discord.com/";
+            
+
+    // Create the line break element
+    var lineBreak = document.createElement("br");
+
+    // Create the "Data gathered from:" text node
+    var dataText = document.createTextNode("Data gathered from: ");
+
+    // Create the link element for NHL.com
+    var nhlLink = document.createElement("a");
+    nhlLink.href = "https://www.nhl.com/";
+    nhlLink.className = "text-white ps-1";
+    nhlLink.textContent = "https://www.nhl.com/";
+
+    window.addEventListener('load',() => {
+        div = document.getElementById("textDiv");
+        let location = window.location.href;
+        let lang = location.split('/').slice(-1)[0]
+        if(lang === "FR"){
+            document.getElementById('language').value="FR"
+            document.getElementById("home").innerHTML = "Accueil"
+            document.getElementById("search").innerHTML = "Recherche"
+            div.innerHTML = "";
+            helpText = document.createTextNode("Besoin d'aide? Visitez notre discord:");
+            dataText = document.createTextNode("Les données recueillies auprès de: ");
+            // Append the elements to the helpDiv
+            div.appendChild(helpText);
+            div.appendChild(discordLink);
+            div.appendChild(lineBreak);
+            div.appendChild(dataText);
+            div.appendChild(nhlLink);
+        } else if(lang === "EN"){
+            document.getElementById("home").innerHTML = "Home"
+            document.getElementById("search").innerHTML = "Search"
+            div.innerHTML = "";
+            helpText = document.createTextNode("Need help? Visit our discord: ");
+            dataText = document.createTextNode("Data gathered from: ");
+            // Append the elements to the helpDiv
+            div.appendChild(helpText);
+            div.appendChild(discordLink);
+            div.appendChild(lineBreak);
+            div.appendChild(dataText);
+            div.appendChild(nhlLink);
+        } 
+    });
+
+    useEffect(() => {
+        div = document.getElementById("textDiv");
+        // Custom code to be executed when the link changes
+        console.log('Link has changed');
+        // You can perform additional actions here
+        if(window.location.href.split('/').slice(-1)[0] === 'EN'){
+            document.getElementById("home").innerHTML = "Home"
+            document.getElementById("search").innerHTML = "Search"
+            div.innerHTML = "";
+            helpText = document.createTextNode("Need help? Visit our discord: ");
+            dataText = document.createTextNode("Data gathered from: ");
+            // Append the elements to the helpDiv
+            div.appendChild(helpText);
+            div.appendChild(discordLink);
+            div.appendChild(lineBreak);
+            div.appendChild(dataText);
+            div.appendChild(nhlLink);
+            
+        } else if(window.location.href.split('/').slice(-1)[0]  === 'FR'){
+            document.getElementById("home").innerHTML = "Accueil"
+            document.getElementById("search").innerHTML = "Recherche"
+            div.innerHTML = "";
+            helpText = document.createTextNode("Besoin d'aide? Visitez notre discord:");
+            dataText = document.createTextNode("Les données recueillies auprès de: ");
+            // Append the elements to the helpDiv
+            div.appendChild(helpText);
+            div.appendChild(discordLink);
+            div.appendChild(lineBreak);
+            div.appendChild(dataText);
+            div.appendChild(nhlLink);
+        } else{
+            document.getElementById('language').value="EN"
+            document.getElementById("home").innerHTML = "Home"
+            document.getElementById("search").innerHTML = "Search"
+            document.getElementById('language').value="EN"
+            div.innerHTML = '';
+            // Append the elements to the helpDiv
+            div.appendChild(helpText);
+            div.appendChild(discordLink);
+            div.appendChild(lineBreak);
+            div.appendChild(dataText);
+            div.appendChild(nhlLink);
+        }
+    }, [locat]);
 
     const langChange = (event) => {
         let location = window.location.href;
         let lang = location.split('/').slice(-1)[0]
+
         if(lang === 'EN' || lang === 'FR'){
             location = location.slice(0, -2);
-            if(lang === 'EN'){
-                document.getElementById("home").innerHTML = "Accueil"
-                document.getElementById("search").innerHTML = "Recherche"
-            } else if(lang === 'FR'){
-                document.getElementById("home").innerHTML = "Home"
-                document.getElementById("search").innerHTML = "Search"
-            }
-        }
+        } 
         if(location.split('/').slice(-1)[0] === ''){
             window.location = location + event.target.value;
+        } else {
+            document.getElementById('language').value="EN"
         }
-        
+
     }; 
 
     
@@ -39,7 +137,7 @@ const Navbar = () => {
                 
             </div>
             <div className="navbar-nav navbar-right me-4">
-                <select className="nav-item nav-link active" style={{ float: 'right' }} onChange={langChange}>
+                <select className="nav-item nav-link active" style={{ float: 'right' }} onChange={langChange} id="language">
                         <option style={{color: "black"}} selected>EN</option>
                         <option style={{color: "black"}}>FR</option>
                 </select>
